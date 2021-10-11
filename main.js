87a76bfa01fafc01588766f2053546d0cdf79dcb
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2021-10-11 21:48:42 +0200

    basic command handler

--- main.js
+++ main.js
@@ -2,14 +2,31 @@ const { Client, Intents } = require('discord.js');
 const { Settings } = require("./util/settings")
 
 // Setting up intents and bot
-const intents = [ Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES ]
+const intents = [ 
+    Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS,
+]
 const bot = new Client( {intents: intents} );
 
 
+//on bot ready
 bot.once("ready", () => {
     console.log(`${bot.user.username} is ready`);
 });
 
+
+//on message
+bot.on("messageCreate", async message => {
+    if(message.author.bot) return;
+
+    const PREFIX = "!pb "
+
+    console.log(`add xp to ${message.author.username}`);
+
+    if(!message.content.startsWith(PREFIX) || !message.content.startsWith(`<@!${bot.user.id}>`)) return;
+    
+    console.log("mentioned");
+
+});
 
 //Logging in bot
 bot.login(Settings.token);
