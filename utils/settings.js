a99753b9edc5ea96bb897e279d6c1b2af8f2032e
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2021-10-12 21:18:03 +0200

    edit: small edit to settings.js

--- utils/settings.js
+++ utils/settings.js
@@ -2,8 +2,9 @@ const dotenv = require("dotenv");
 
 dotenv.config( { path: "./data/.env"});
 
-var Settings = {
-    token : process.env.BOT_TOKEN
+const Settings = {
+    token : process.env.BOT_TOKEN,
+    sqlDebug : Boolean(process.env.SQL_DEBUG),
 }
 
 module.exports = {Settings};
