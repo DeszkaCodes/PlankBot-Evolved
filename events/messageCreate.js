1d1382284be475e2cf433c21b5214287abe4d0b3
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2021-10-12 22:53:12 +0200

    feature: custom prefix for premium servers

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,11 +1,50 @@
 const Database = require("../utils/database");
 const { Op }= require("sequelize");
 const Random = require("../utils/betterRandom");
+const { StartsWithStringArray } = require("../utils/stringFunctions");
 
 const expConstraints = {
     min: 10,
     max: 15
-}
+};
+
+async function HandleExp(message){
+    //EXP Handling
+    try{
+        //Try to fetch the user's data or create it
+        const localData = await Database.LocalData.findCreateFind({
+            where: { [Op.and]: { SERVERID: message.guildId, ID: message.author.id } },
+            defaults: { SERVERID: message.guildId, ID: message.author.id, }
+        });
+
+        //Increment the exp of the user by a random amount
+        const affectedRows = await Database.LocalData.increment(
+            { EXP:  +Random.RandomInt(expConstraints.min, expConstraints.max) },
+            { where: { [Op.and]: { SERVERID: message.guildId, ID: message.author.id } } }
+        );
+
+        return affectedRows.EXP;
+
+    }catch(error){
+        console.error(error.name);
+        console.error(error.message);
+        console.error(error.stack);
+    }
+};
+
+async function GetPrefix(message){
+    const [serverData, created] = await Database.ServerData.findCreateFind({
+        where: { ID: message.guildId },
+        defaults: { ID: message.guildId, PREFIX: null },
+    })
+
+    prefixes = ["!pb "]
+    
+    if(serverData.PREMIUM)
+        if(serverData.PREFIX != undefined) prefixes.push(serverData.PREFIX + " ");
+
+    return prefixes;
+};
 
 module.exports = {
     name: "messageCreate",
@@ -13,31 +52,13 @@ module.exports = {
 
         if(message.author.bot) return;
 
-        const PREFIX = "!pb ";
+        const PREFIX = await GetPrefix(message);
 
-        //EXP Handling
-        try{
-            //Try to fetch the user's data or create it
-            const localData = await Database.LocalData.findOrCreate({
-                where: { [Op.and]: { SERVERID: message.guildId, ID: message.author.id } },
-                defaults: { SERVERID: message.guildId, ID: message.author.id, }
-            });
-
-            //Increment the exp of the user by a random amount
-            const affectedRows = await Database.LocalData.increment(
-                { EXP:  +Random.RandomInt(expConstraints.min, expConstraints.max) },
-                { where: { [Op.and]: { SERVERID: message.guildId, ID: message.author.id } } }
-            );
-
-        }catch(error){
-            console.error(error.name);
-            console.error(error.message);
-            console.error(error.stack);
-        }
+        await HandleExp(message);
 
 
-        if(!(message.content.startsWith(PREFIX) || message.content.startsWith(`<@!${bot.user.id}>`))) return;
+        if(!(StartsWithStringArray(message.content, PREFIX) || message.content.startsWith(`<@!${bot.user.id}>`))) return;
 
         await message.reply("got it");
     }
-}
+};
