ea3166927be77540edecc742df70fd7b3105601c
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2021-10-13 18:34:44 +0200

    feature: new command

---
+++ commands/broadcast.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "broadcast",
+    async execute(bot, message, text){
+
+        text = text.join(" ");
+
+        const application = await bot.application.fetch();
+        const ownerTeam = application.owner;
+
+        if(!ownerTeam.members.has(message.author.id)){
+            await message.reply("Nincs jogod végrehajtani ezt a parancsot.")
+            return;
+        }
+
+        const o2guilds = await bot.guilds.fetch();
+
+        const inviteLink = "https://discord.com/api/oauth2/authorize?client_id=890537505605181480&permissions=8&scope=bot";
+
+        for (const [key, o2guild] of o2guilds){
+            const guild = await o2guild.fetch();
+
+            const embed = new Discord.MessageEmbed()
+                .setTitle("FONTOS ÜZENET")
+                .setAuthor(bot.user?.username, inviteLink, bot.user?.displayAvatarURL())
+                .setColor("#e00000")
+                .setDescription(text)
+                .setTimestamp();
+
+            guild.systemChannel?.send({embeds: [embed], content: "@everyone"});
+        }
+
+        console.log(`Global message sent to every possible guild:\n${text}\n\n`);
+    }
+};
