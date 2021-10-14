8268528700d4d5020cdbbb6eaa419087ed5444d5
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2021-10-14 18:28:23 +0200

    update: user fetch

--- utils/userAuthentication.js
+++ utils/userAuthentication.js
@@ -2,7 +2,7 @@
  * Place for all the functions that are connected to user objects
 */
 
-const { ClientApplication, User } = require("discord.js");
+const { ClientApplication, User, MessageMentions: { USERS_PATTERN } } = require("discord.js");
 
 async function IsOwner(bot, user){
 
@@ -23,6 +23,27 @@ async function IsOwner(bot, user){
     return false;
 }
 
+async function UserFromMention(bot, mention){
+    const matches = mention.match(USERS_PATTERN);
+
+    // If supplied variable was not a mention, matches will be null instead of an array.
+    if (!matches) return;
+
+    // The first element in the matches array will be the entire mention, not just the ID,
+    // so use index 1.
+    const id = matches[1];
+}
+
+async function GetUser(bot, id){
+    try{
+        return bot.users.cache.get(id);
+    }catch(err){
+        return;
+    }
+}
+
 module.exports = {
-    IsOwner
+    IsOwner,
+    GetUser,
+    UserFromMention
 };
