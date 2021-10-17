c03e3c407f351b47c0742a3f29e133040044d6ee
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2021-10-17 21:24:05 +0200

    feature: typescript integratio

--- utils/clientHelper.ts
+++ utils/clientHelper.ts
@@ -1,4 +1,4 @@
-import { Client, PermissionResolvable } from "discord.js";
+import { Client, PermissionResolvable, User, Team, ClientApplication } from "discord.js";
 
 export function GetInvite(bot: Client, flags: Array<PermissionResolvable>) : string{
     const invite: string = bot.generateInvite({
@@ -7,4 +7,25 @@ export function GetInvite(bot: Client, flags: Array<PermissionResolvable>) : str
     });
 
     return invite;
+}
+
+export async function IsOwner(bot: Client, user: User): Promise<boolean>{
+
+    //fetches the bot's owner team
+    const application: (ClientApplication | undefined) = await bot.application?.fetch();
+
+    if(application instanceof ClientApplication){
+        const owner: (User | Team | null) = application.owner;
+        
+        if(owner instanceof Team){
+            if(owner.members.has(user.id))
+                return true;
+            
+        }else if(owner instanceof User){
+            if(owner.id == user.id)
+                return true;
+        }
+    }
+        
+        return false;
 }
