0e8692447750d4d2e4ee00cf6e7991692a4ec8e6
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2021-10-17 21:30:25 +0200

    update: isowner command

--- commands/amIAnOwner.js
+++ commands/amIAnOwner.js
@@ -1,9 +1,9 @@
-//const { IsOwner } = require("../utils/clientHelper.js");
+const { IsOwner } = require("../build/clientHelper");
 
 module.exports = {
     name: "amianowner?",
     async execute(bot, message, ...args){
-        //const isOwner = await IsOwner(bot, message.author);
+        const isOwner = await IsOwner(bot, message.author);
 
         await message.reply(isOwner ? "Igen, bot tulajdonos vagy" : "Nem nem vagy bot tulajdonos");
     }
