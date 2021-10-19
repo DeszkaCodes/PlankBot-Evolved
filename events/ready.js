596144fe487366cf94c3691762906b589a0a3cbc
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2021-10-19 22:53:16 +0200

    UPDATE: bot activity changer

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,33 @@ const { Time } = require("../utils/time");
 const { Op } = require("sequelize");
 const chalk = require("chalk");
 const Config = require("../data/config.json")
+const { ArrayCycle } = require("../utils/array");
+
+async function SetupIntervals(bot){
+
+    //Deletes already expired cooldowns from database
+    const TrimCommandCooldownInterval = setInterval(function() {
+        for (let command of bot.commands.filter(comm => comm.cooldown.IsOn)) {
+            Database.CommandCooldowns.destroy({
+                where: {
+                    [Op.and]: [
+                        { COMMAND: command[0] },
+                        { ENDTIME: {
+                            [Op.lt]: Date.now()
+                        }}
+                    ]
+                }
+            });
+        }
+    }, Config.intervals.cooldownTrim);
+
+    //Changes presence of bot
+    const cycle = new ArrayCycle(Config.botUser.activity);
+    const BotPresenceInterval = setInterval(async function() {
+        const activity = cycle.next();
+        bot.user?.setActivity({ name: activity.name, type: activity.type });
+    }, Config.intervals.changePresence);
+}
 
 module.exports = {
     name: "ready",
@@ -11,20 +38,8 @@ module.exports = {
         await Database.Init();
 
         //Sets functions to be run periodically
-        const TrimCommandCooldownInterval = setInterval(function() {
-            for (let command of bot.commands.filter(comm => comm.cooldown.IsOn)) {
-                Database.CommandCooldowns.destroy({
-                    where: {
-                        [Op.and]: [
-                            { COMMAND: command[0] },
-                            { ENDTIME: {
-                                [Op.lt]: Date.now()
-                            }}
-                        ]
-                    }
-                });
-            }
-        }, Config.intervals.cooldownTrim);
+        SetupIntervals(bot);
+        
         console.log(chalk.greenBright(`${bot.user.username} is ready`));
     }
 };
