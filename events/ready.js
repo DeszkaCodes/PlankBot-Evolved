a242f41bd9a5ab5b0f6f736cee36096991b3942a
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2021-10-19 21:07:50 +0200

    UPDATE: periodically check cooldown

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,30 @@
 const Database = require("../utils/database");
-const { Colors } = require("../utils/ansi")
+const { Time } = require("../utils/time");
+const { Op } = require("sequelize");
+const chalk = require("chalk");
+const Config = require("../data/config.json")
 
 module.exports = {
     name: "ready",
     once: true,
-    execute(bot){
-        Database.Init();
+    async execute(bot){
+        await Database.Init();
 
-        console.log(`${Colors.Text.Green}${bot.user.username} is ready${Colors.Text.White}`);
+        //Sets functions to be run periodically
+        const TrimCommandCooldownInterval = setInterval(function() {
+            for (let command of bot.commands.filter(comm => comm.cooldown.IsOn)) {
+                Database.CommandCooldowns.destroy({
+                    where: {
+                        [Op.and]: [
+                            { COMMAND: command[0] },
+                            { ENDTIME: {
+                                [Op.lt]: Date.now()
+                            }}
+                        ]
+                    }
+                });
+            }
+        }, Config.intervals.cooldownTrim);
+        console.log(chalk.greenBright(`${bot.user.username} is ready`));
     }
 };
