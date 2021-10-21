74a686f1aa9d7e334baa1753ca4dd64e92a9bd34
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2021-10-21 17:36:15 +0200

    CREATED: help cmd

---
+++ commands/help_commands.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "segítség",
+    cooldown: { IsOn: false, Time: null }, // Time given in milliseconds
+    /*ARGUMENTS:
+    */
+    async execute(bot, message, ...args){
+        
+    }
+};
