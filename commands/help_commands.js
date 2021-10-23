4a03cc36240a5a1d58fa58a2a6232b9693caf767
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2021-10-23 16:08:34 +0200

    UPDATE: finished help system

--- commands/help_commands.js
+++ commands/help_commands.js
@@ -1,8 +1,9 @@
 const { MessageEmbed } = require("discord.js");
 const { errorEmbed, EmbedBotAuthor } = require("../utils/embed");
-const Config = require("../data/config.json")
+const Config = require("../data/config.json");
+const { Capitalize } = require("../utils/stringFunctions.js");
 
-function ArgumentsToString(command){
+function HowToUse(command){
 
     let text = `${Config.prefix} ${command.name}`;
 
@@ -12,59 +13,86 @@ function ArgumentsToString(command){
 
     for(let argument of command.help.arguments){
         if(argument.optional)
-            text += " <" + argument.name + " - " + argument.description + ">";
+            text += " <" + argument.name + ">";
         else
-            text += ` [${argument.name} - ${argument.description}]`
+            text += ` [${argument.name}]`
     };
 
     return text;
-}
+};
 
-module.exports = {
-    name: "segítség",
-    cooldown: { IsOn: false, Time: null }, // Time given in milliseconds
-    /*ARGUMENTS:
-    */
-    async execute(bot, message, ...args){
-        //if no command given exterminate the process
-        if(args.length <= 0){
-            const embed = errorEmbed(
-                bot, "Nem adtad meg a parancsot, amiről segítséget kérsz."
-            );
+async function HasCommand(bot, message, args){
+    const helpCommand = args[0];
 
-            await message.reply({embeds: [embed]});
-
-            return;
-        };
-
-        const helpCommand = args[0][0];
-
-        console.log(bot.commands)
-        console.log(helpCommand)
-
-        //search for the command
-        if(!bot.commands.has(helpCommand)){
-            const embed = errorEmbed(
-                bot, "A megadott parancs nem található.",
-                [{name: "Megadott parancs",value: helpCommand}]
-            );
-
-            await message.reply({embeds:[embed]});
-
-            return;
-        }
-        const command = bot.commands.get(helpCommand);
-
-        const embed = EmbedBotAuthor(bot)
-            .setTitle("Parancs súgó")
-            .setTimestamp()
-            .setColor(Config.embed.colors.default);
-
-        const argsText = ArgumentsToString(command);
-        const description = command.help.description;
-
-        embed.setDescription(`${description}\n${argsText}`);
+    //search for the command
+    if(!bot.commands.has(helpCommand)){
+        const embed = errorEmbed(
+            bot, "A megadott parancs nem található.",
+            [{name: "Megadott parancs",value: helpCommand}]
+        );
 
         await message.reply({embeds:[embed]});
+
+        return;
+    }
+    const command = bot.commands.get(helpCommand);
+
+    const embed = EmbedBotAuthor(bot)
+        .setTitle(`Parancs súgó - ${command.name}`)
+        .setTimestamp()
+        .setColor(Config.embed.colors.default)
+        .setDescription(command.help.description)
+        .addFields([
+            {name: "Használat", value: HowToUse(command), inline: false },
+            {name: "Cooldown", value: `${command.cooldown.IsOn ? "Van" : "Nincs"}`, inline: false },
+        ]);
+
+    if(command.help.arguments.length > 0){
+        embed.addField(Config.embed.empty, "**Paraméterek:**", false)
+        for(let argument of command.help.arguments){
+            embed.addField(
+                `${Capitalize(argument.name)} ${argument.optional ? "*(opcionális)*" : ""}`,
+                Capitalize(argument.description),
+                false
+            );
+        };
+    };
+
+    await message.reply({embeds:[embed]});
+};
+
+async function NoCommandsGiven(bot, message, args){
+    const commands = bot.commands;
+
+    let arguments = "";
+
+    for(let command of commands){
+        arguments += `**${command[1].name}**: ${command[1].help.description}\n`;
+    };
+    
+
+    const embed = EmbedBotAuthor(bot)
+        .setTitle("Parancs súgó")
+        .setColor(Config.embed.colors.default)
+        .setTimestamp()
+        .setDescription("Összes parancs listája.")
+        .addField(`Parancsok\n${Config.embed.empty}`, arguments);
+
+    await message.reply({embeds:[embed]});
+};
+
+module.exports = {
+    name: "súgó",
+    cooldown: { IsOn: false, Time: null }, // Time given in milliseconds
+    help:{
+        arguments: [{name: "parancs", description: "a keresett parancs neve", optional: true}],
+        description: "Kiírja az összes parancsot, illetve megmutatja a parancsok paramétereit."
+    },
+    async execute(bot, message, args){
+        //if no command given exterminate the process
+        if(args.length <= 0)
+            NoCommandsGiven(bot, message, args);
+        else
+            HasCommand(bot, message, args);
     }
 };
--- utils/stringFunctions.js
+++ utils/stringFunctions.js
@@ -2,6 +2,11 @@ function StartsWithStringArray(str, starts){
     return starts.some(item => str.startsWith(item));
 }
 
+function Capitalize(str){
+    return str[0].toUpperCase() + str.substring(1);
+}
+
 module.exports = {
-    StartsWithStringArray
+    StartsWithStringArray,
+    Capitalize
 };
