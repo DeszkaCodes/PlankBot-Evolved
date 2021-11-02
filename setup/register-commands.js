7675da819bf5f3671230bbde8baf53f0a8504dc6
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2021-11-02 19:19:00 +0100

    UPDATED: register slash

--- setup/register-commands.js
+++ setup/register-commands.js
@@ -2,6 +2,7 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { Settings } = require('../utils/settings');
 const fs = require('fs');
+const chalk = require("chalk");
 
 function Register(){
     commands = []
@@ -16,13 +17,12 @@ function Register(){
         commands.push(command.data.toJSON());
     }
 
-
     const rest = new REST({ version: '9' }).setToken(Settings.token);
 
 
     (async () => {
         try {
-            console.log('Started refreshing application (/) commands.');
+            console.log(chalk.green('Started refreshing application (/) commands.'));
 
             if(Settings.debug){
                 await rest.put(
@@ -37,9 +37,9 @@ function Register(){
                 );
             }
 
-            console.log('Successfully reloaded application (/) commands.');
+            console.log(chalk.greenBright('Successfully reloaded application (/) commands.'));
         } catch (error) {
-            console.error(error);
+            console.error(chalk.redBright(error));
         }
     })();
 };
