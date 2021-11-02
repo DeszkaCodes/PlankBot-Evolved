660b641a5eaaa12adf1f7badaabf9738760f4361
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2021-11-02 16:44:42 +0100

    DELETED: sqlDebug token

--- utils/settings.js
+++ utils/settings.js
@@ -3,8 +3,7 @@ const dotenv = require("dotenv");
 dotenv.config( { path: "./data/.env"});
 
 const Settings = {
-    token : process.env.BOT_TOKEN,
-    sqlDebug : Boolean(process.env.SQL_DEBUG),
+    token : process.env.BOT_TOKEN
 }
 
 module.exports = {Settings};
