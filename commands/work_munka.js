088dd76628b858089b22edefcaee77aef78a325b
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2021-11-02 19:18:16 +0100

    CONVERT: work command

--- commands/work_munka.js
+++ commands/work_munka.js
@@ -4,33 +4,32 @@ const { LocalData } = require("../utils/database");
 const { Op } = require("sequelize");
 const Config = require("../data/config.json");
 const { Time } = require("../utils/time");
+const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
-    name: "munka",
+    data : new SlashCommandBuilder()
+        .setName("munka")
+        .setDescription("Kockázatmentes pénzszerzési lehetőség."),
     cooldown: { IsOn: true, Time: Time.minute * 5 }, // Time given in milliseconds
-    help: {
-        arguments: [],
-        description: "Egy gyors és kockázatmentes pénzszerzési lehetőség."
-    },
-    async execute(bot, message, ...args){
-
+    async execute(bot, interaction){
+        
         const [data, found] = await LocalData.findCreateFind({
-            where: { [Op.and]: { SERVERID: message.guildId, ID: message.author.id } },
-            defaults: { SERVERID: message.guildId, ID: message.author.id }
+            where: { [Op.and]: { SERVERID: interaction.guildId, ID: interaction.user.id } },
+            defaults: { SERVERID: interaction.guildId, ID: interaction.user.id }
         });
 
         const payment = RandomInt(Config.Jobs.Work.reward.min, Config.Jobs.Work.reward.max);
         
         const affectedRows = await LocalData.increment(
             { BALANCE: +payment },
-            { where: { [Op.and]: {SERVERID: message.guild.id, ID: message.author.id } } }
+            { where: { [Op.and]: {SERVERID: interaction.guild.id, ID: interaction.user.id } } }
         );
 
         if(affectedRows <= 0) return;
 
         const embed = new MessageEmbed()
             .setTitle("Szorgos dolgozó")
-            .setAuthor(message.guild.name, message.guild.iconURL())
+            .setAuthor(interaction.guild.name, interaction.guild.iconURL())
             .setDescription("Megjött a várva várt fizetésed.")
             .setTimestamp()
             .setColor(Config.embed.colors.money)
@@ -43,6 +42,6 @@ module.exports = {
             .setFooter("Hogy kipihend magad nem használhatod ezt a parancsot 5 percig.\n")
 
 
-        message.reply({embeds: [embed], ephemeral: true});
+        interaction.reply({embeds: [embed], ephemeral: true});
     }
 };
