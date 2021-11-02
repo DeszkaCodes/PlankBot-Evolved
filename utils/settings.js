f8f040568bfe8f341a3554555a87a63f8b4cd564
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2021-11-02 16:54:22 +0100

    UPDATE: .env

--- utils/settings.js
+++ utils/settings.js
@@ -4,7 +4,9 @@ dotenv.config( { path: "./data/.env"});
 
 const Settings = {
     token : process.env.BOT_TOKEN,
-    debug : process.env.DEBUG
+    debug : process.env.DEBUG,
+    clientId : process.env.CLIENT_ID,
+    debug_guildId : process.env.DEBUG_GUILD_ID
 }
 
 module.exports = {Settings};
