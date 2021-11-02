661ec7b737075d2e37ba2533327caef1f4747063
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2021-11-02 19:18:25 +0100

    CONVERT: ping command

--- commands/debug_ping.js
+++ commands/debug_ping.js
@@ -4,7 +4,8 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Ellenőrzi, hogy a bot megkapja az üzeneteid.'),
-	async execute(interaction) {
-		await interaction.reply('Pong!');
+	cooldown: { IsOn: false, Time: null }, // Time given in milliseconds
+	async execute(bot, interaction) {
+		interaction.reply('Pong!');
 	},
 };
