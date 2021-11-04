bd5dab43bf5c29522fe25b37088fe75afb17388b
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2021-11-04 21:34:28 +0100

    UPDATED: better ping

--- commands/debug-ping.js
+++ commands/debug-ping.js
@@ -1,11 +1,37 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require("discord.js");
+
+//TODO: make it fancy
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Ellenőrzi, hogy a bot megkapja az üzeneteid.'),
+		.setDescription('Visszaküldi a bot válaszidejét.'),
 	cooldown: { IsOn: false, Time: null }, // Time given in milliseconds
 	async execute(bot, interaction) {
-		interaction.reply('Pong!');
+		const embed = new MessageEmbed()
+				.setTitle("Válaszidő")
+				.setDescription("A bot válaszidejének lekérdezése")
+				
+				.setTimestamp();
+
+		interaction.reply({ content: "Bot pingelése...", fetchReply: true }).then(async (msg) =>{
+			const ping = msg.createdTimestamp - interaction.createdTimestamp;
+
+			let color = "#00ff00";
+
+			if(ping > 350)
+				color = "#ff0000";
+			else if(ping > 300)
+				color = "#ff8c00";
+			else if(ping > 250)
+				color = "#ffcc00";
+			
+
+			embed.addField("Válaszidő", `*${ping}* ms`)
+			embed.setColor(color)
+
+			msg.edit({ embeds: [ embed ] });
+		});
 	},
 };
